55de126a3d4b87efe28589a81b5e73cc5f27c539
LuK1337 <priv.luk@gmail.com>
2025-01-18 18:33:32 +0100

    Move WfdCommon boot jar declaration to vendor
    Change-Id: I8fa2c0f64692873b432cc252ec88b388a32023a1

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -834,3 +834,6 @@ PRODUCT_PACKAGES += \
     system_ext_priv-app_ims_lib_arm64_libimscamera_jni_so \
     system_ext_priv-app_ims_lib_arm64_libimsmedia_jni_so \
     vendor_bin_expat
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
